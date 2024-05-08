e3cb089be980b4765b419287eace93e274759c12
isurika-2001 <cb009957@students.apiit.lk>
2024-05-08 08:22:49 +0530

    vercel configs

--- server/src/index.js
+++ server/src/index.js
@@ -1,26 +1,37 @@
 // script to start the server
 require("dotenv").config();
 const bodyParser = require("body-parser");
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 
 // project imports
 const connectToDatabase = require("../database");
 
 // Routes
-const studentRoutes = require('./routes/student');
-const courseRoutes = require('./routes/course');
-const batchRoutes = require('./routes/batch');
-const courseRegistrationRoutes = require('./routes/course_registration');
-const user_typeRoutes = require('./routes/user_type');
-const userRoutes = require('./routes/user');
+const studentRoutes = require("./routes/student");
+const courseRoutes = require("./routes/course");
+const batchRoutes = require("./routes/batch");
+const courseRegistrationRoutes = require("./routes/course_registration");
+const user_typeRoutes = require("./routes/user_type");
+const userRoutes = require("./routes/user");
 
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(express.json());
+app.use(
+  express.json({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+// default route
+app.get("/", (req, res) => {
+  res.send("Welcome to the student management system");
+});
 
 // Use body-parser middleware
 app.use(bodyParser.json());
@@ -30,12 +41,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 connectToDatabase();
 
 // Routes
-app.use('/api', studentRoutes);
-app.use('/api', courseRoutes);
-app.use('/api', batchRoutes);
-app.use('/api', courseRegistrationRoutes);
-app.use('/api', user_typeRoutes);
-app.use('/api', userRoutes);
+app.use("/api", studentRoutes);
+app.use("/api", courseRoutes);
+app.use("/api", batchRoutes);
+app.use("/api", courseRegistrationRoutes);
+app.use("/api", user_typeRoutes);
+app.use("/api", userRoutes);
 
 // Start the server
 app.listen(PORT, () => {
