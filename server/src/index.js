925dab92891fe9105552813a1db7358eeed60bda
isurika-2001 <cb009957@students.apiit.lk>
2024-05-08 08:52:25 +0530

    index

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   express.json({
-    origin: "http://localhost:3000",
+    origin: "https://ums-polymath.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
