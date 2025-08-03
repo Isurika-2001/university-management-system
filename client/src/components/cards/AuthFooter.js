0a5595d7cbcfeba97b1bd164cc6342b50ce147ea
IsurikaKN <isurika@kernernorland.com>
2025-08-03 12:29:00 +0530

    changed to correct

--- client/src/components/cards/AuthFooter.js
+++ client/src/components/cards/AuthFooter.js
@@ -17,7 +17,7 @@ const AuthFooter = () => {
         <Typography variant="subtitle2" color="secondary" component="span">
           &copy; Student Registration System By&nbsp;
           <Typography component={Link} variant="subtitle2" href="https://codedthemes.com" target="_blank" underline="hover">
-            Isurika Samarakoon
+            Concord Tech Solutions
           </Typography>
         </Typography>
       </Stack>
